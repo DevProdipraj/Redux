606b4d862d7c87a031e12a30d1bdb97d3b6317c9
Prodip Rajbongshi <devprodipraj@gmail.com>
2025-02-11 23:06:38 +0600

    add update file

--- src/component/AddTodo.jsx
+++ src/component/AddTodo.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useRef } from "react";
 
 const AddTodo = () => {
-  return (
-    <div className='bg-gray-400 w-1/3 m-auto p-4 rounded-md mt-5 '>
-      <input type="text" name="todos" id="" />
-      <button className='cursor-pointer' type="button">Add</button>
-    </div>
-  )
-}
+  const userText = useRef(null);
 
-export default AddTodo
+  const handleBtn = () => {
+    console.log(userText.current.value);
+  };
+
+  return (
+    <div className="bg-gray-400 w-1/3 m-auto p-4 rounded-md mt-5">
+      <input
+        ref={userText}
+        type="text"
+        name="todos"
+        id=""
+        className="p-2 border rounded-md w-full"
+      />
+      <button
+        onClick={handleBtn}
+        className="cursor-pointer mt-2 bg-blue-500 text-white px-4 py-2 rounded-md"
+        type="button"
+      >
+        Add
+      </button>
+    </div>
+  );
+};
+
+export default AddTodo;
--- src/features/userSlicce.jsx
+++ src/features/userSlicce.jsx
@@ -1,18 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const usersSlice = createSlice({
-    name: "users",
-    initialState: [],
-    reducers: {
-        addUser(state, action) { },
-        removeUser(state, action) { },
-        removeAlluser(state, action) { },
-    }
-    
-})
+  name: "users",
+  initialState: [],
+  reducers: {
+    addUser(state, action) {},
+    removeUser(state, action) {},
+    removeAlluser(state, action) {},
+  },
+});
 
-console.log(usersSlice.actions);
-
-export { usersSlice }
-
-
+export { usersSlice };
