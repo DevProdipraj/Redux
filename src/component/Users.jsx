f37d3b9e36d3d3eb24d0e0ed5aa9df9565ba5539
DevProdipraj <devprodipraj@gmail.com>
2025-02-16 15:54:46 +0600

    add remove all user btn

--- src/component/Users.jsx
+++ src/component/Users.jsx
@@ -38,14 +38,16 @@ const Users = () => {
         ))}
       </ul>
 
+        <div className="w-1/3 m-auto text-right">
       {users.length > 0 && (
         <button
           onClick={handleRemoveAll}
-          className="mt-4 bg-red-600 text-white px-4 py-2 rounded-md transition-all duration-500 hover:bg-red-800"
+          className="mt-4 bg-red-600 text-white px-4 py-2 rounded-md transition-all duration-500 hover:bg-red-800 text-right cursor-pointer"
         >
           Remove All Users
         </button>
       )}
+      </div>
     </div>
   );
 };
